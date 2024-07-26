3580193f3910d500e8e30919ab6e36ebe2cfcbad
Sami <47335566+ssaammii5@users.noreply.github.com>
2024-07-26 20:35:36 +0600

    Initial commit

---
+++ app/src/main/java/com/example/myshoppinglistapp/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.myshoppinglistapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myshoppinglistapp.ui.theme.MyShoppingListAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
+        setContent {
+            MyShoppingListAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize(),
+                        verticalArrangement = Arrangement.Center
+                        ) {
+                        Button(onClick = { },
+                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                            ) {
+                            Text("Add Item")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
